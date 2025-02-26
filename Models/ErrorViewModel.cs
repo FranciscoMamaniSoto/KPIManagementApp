b75a80fb27caf1deb1bbe8c3a1bde0fc60dc15b4
fmamani <fmamani@PRLP21HP127358S>
2025-02-26 09:45:23 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KPIManagementApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
